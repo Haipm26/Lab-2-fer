3f900c3dededf1ec1f88e97086cad304e9f07e0c
h41 <haiphamminh2k6@gmail.com>
2026-06-10 11:19:47 +0700

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,30 @@
 import { Col, Container, Row } from 'react-bootstrap'
 
 function Footer() {
-  return (
-    <footer className="site-footer" id="contact">
-      <Container>
-        <Row>
-          <Col md={3}>
-            <strong>Student Name</strong>
-            <span>Pham Minh Hai</span>
-          </Col>
-          <Col md={3}>
-            <strong>Student ID</strong>
-            <span>Update your student ID</span>
-          </Col>
-          <Col md={3}>
-            <strong>Class Name</strong>
-            <span>FER Lab</span>
-          </Col>
-          <Col md={3}>
-            <strong>Email</strong>
-            <span>student@example.com</span>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  )
+    return (
+        <footer className="site-footer" id="contact">
+            <Container>
+                <Row>
+                    <Col md={3}>
+                        <strong>Student Name</strong>
+                        <span>Pham Minh Hai</span>
+                    </Col>
+                    <Col md={3}>
+                        <strong>Student ID</strong>
+                        <span>HE200055</span>
+                    </Col>
+                    <Col md={3}>
+                        <strong>Class Name</strong>
+                        <span>FER</span>
+                    </Col>
+                    <Col md={3}>
+                        <strong>Email</strong>
+                        <span>haiphamminh2k6@gmail.com</span>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default Footer
